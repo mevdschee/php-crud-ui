a0d77370d46632879b30c0a327207c75c367216d
Maurits van der Schee <maurits@vdschee.nl>
2019-07-19 00:02:35 +0200

    refactor wip

--- src/index.php
+++ src/index.php
@@ -2,7 +2,6 @@
 use Tqdev\PhpCrudApi\RequestFactory;
 use Tqdev\PhpCrudApi\ResponseUtils;
 use Tqdev\PhpCrudUi\Config;
-use Tqdev\PhpCrudUi\Ui2;
 
 require '../vendor/autoload.php';
 
@@ -10,6 +9,6 @@ $config = new Config([
     'url' => 'http://localhost:8000/api.php',
 ]);
 $request = RequestFactory::fromGlobals();
-$ui = new Ui2($config);
+$ui = new Ui($config);
 $response = $ui->handle($request);
 ResponseUtils::output($response);
